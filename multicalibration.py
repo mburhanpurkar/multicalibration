9a80684d9dac1b4f05c81c515ad877d46aa6e981
mburhanpurkar <maya.burhanpurkar@gmail.com>
2021-03-24 23:31:01 -0400

    more progress

--- multicalibration.py
+++ multicalibration.py
@@ -2,20 +2,22 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
+
 # Parameters
 r = 2
 d = 5
 C = 100
-n_train, n_valid, n_test = 1000, 100, 100
+n_train, n_valid, n_test = 50, 100, 100
 eps = 0.01
 batch_size = 32
 steps_per_epoch = n_train / batch_size
 n_epochs = 5
 B = round(n_train**(1./r + 1))
 
+
 def get_data(r, d, C, n_train, n_valid, n_test, test=False):
+    """Define the ground truth"""
     n = n_train + n_valid + n_test
-    # Define the ground truth
     w2_star = np.random.rand(r)
     W1_star = np.random.rand(r, d)
     M = w2_star @ W1_star
@@ -36,18 +38,8 @@ def get_data(r, d, C, n_train, n_valid, n_test, test=False):
     return D1, D2, x_valid, y_valid, x_test, y_test
 
 
-"""
-# Let's make sure the code above works! 
-D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test, test=True)
-assert(len(x_valid) == len(y_valid) == n_valid)
-assert(len(x_test) == len(y_test) == n_test)
-assert(len(D1[0]) == len(D1[1]) == n_train // 2)
-assert(len(D2[0]) == len(D2[1]) == n_train // 2)
-assert(not(np.all(np.equal(D1[0], D2[0]))))
-"""
-
-# Define our estimator
 def get_model(d, r, eps):
+    """Define the linear model"""
     inputs = keras.Input(shape=(d,))
     dense = keras.layers.Dense(units=r, 
                                use_bias=False, 
@@ -58,8 +50,10 @@ def get_model(d, r, eps):
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
 
+
 def train_model(model, D1, x_valid, y_valid):
-    # TODO: optimize params
+    """Train the model (with Adam to avoid gradient problems)"""
+    # TODO: optimize params!
     n_epochs = 3
     batch_size = 1
     lr = 0.1
@@ -78,96 +72,107 @@ def train_model(model, D1, x_valid, y_valid):
     return out
 
 
-"""
-# Let's make sure the code above works! 
-D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test)
-model = get_model(d, r, eps)
-print(model.summary())
-mse = train_model(model, D1, x_valid, y_valid)
-"""
-# Maybe make it the identity map. Then we expect it to partition in a 
-# sensible way in x. Then make B = r = 2 so we can plot nicely
-def get_sets(model, D1, C, B, r):
+def split_sets(model, D1, C, B, r):
+    """Split D1 into sets by Linjun's method and get the corresponding intervals"""
     model = keras.Model(inputs=model.input, outputs=model.get_layer('dense').output)
-    xs = np.linspace(0, 1, B + 1)
-    set_ranges = np.empty((1, r, 2))
-    set_ranges[0, :, 0] = -C
-    set_ranges[0, :, 1] = C
+    quantiles_B = np.linspace(0, 1, B + 1)
     sets = [D1]
     quantiles_list = []
     
     for j in range(r):
-        new_ranges = np.tile(set_ranges, (B, 1, 1))
         new_sets = []
         quantiles_tmp = []
-
+        #print("sets j =", j, " -> ", sets)
         for i in range(len(sets)):
-            # Get jth neuron values in second last layer
-            features = model.predict(sets[i][0])[j]
-            quantiles = [-C] + list(np.quantile(features, xs[1:-1])) + [C]
-            print(quantiles)
+            features = model.predict(sets[i][0])[:, j]
+            quantiles = [-C] + list(np.quantile(features, quantiles_B[1:-1])) + [C]
             quantiles_tmp.append(quantiles)
+            sets_tmp = [[] for i in range(B)]
+            sets_tmp_y = [[] for i in range(B)]
+
+            for sample_id in range(len(sets[i][0])):
+                for k in range(B):
+                    if features[sample_id] >= quantiles[k] and features[sample_id] <= quantiles[k + 1]:
+                        sets_tmp[k].append(sets[i][0][sample_id])
+                        sets_tmp_y[k].append(sets[i][1][sample_id])
+                        break
+                    assert(k != B - 1)
 
             for k in range(B):
-                # Update ranges and sets
-                print(i * B + k, j, k)
-                new_ranges[i * B + k, j] = [quantiles[k], quantiles[k + 1]]
-                mask = features > quantiles[k] and features < quantiles[k + 1]
-                xs, ys = np.where(mask, sets[i][0]),  np.where(mask, sets[i][1])
-                new_sets.append((xs, ys))
+                new_sets.append((np.array(sets_tmp[k]), np.array(sets_tmp_y[k])))
 
-        quantlies_list.append(quantiles_tmp)
+        quantiles_list.append(quantiles_tmp)
         sets = new_sets
-        set_ranges = new_ranges
 
-    # Get the number of elements in each set
     set_counts = [len(i[0]) for i in sets]
-
-    return sets, set_ranges, set_counts, quantiles_list
-
-# """
-# Let's make sure the code above works! 
-r = 10
-d = 2
-B = 2
-D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test)
-model = get_model(d, r, eps)
-mse = train_model(model, D1, x_valid, y_valid)
-sets, set_ranges, set_counts, quantiles_list = get_sets(model, D1, C, B, r)
-#"""
+    return sets, set_counts, quantiles_list
 
 
+def get_set(sample, r, model, quantiles_list):
+    """Determine which set a sample is in"""
+    model = keras.Model(inputs=model.input, outputs=model.get_layer('dense').output)
+    features = model.predict(np.reshape(sample, (1, -1)))
 
-# # Use set_ranges to make decision tree
-# def get_set(sample):
-#     features = extractor(sample)
-#     indices = []
-#     for j in range(r):
-#         for i in range(B):
-#             if features[j] > quantiles_list[j][i] and features[j] < quantiles_list[j][i + 1]:
-#                 indices.append[i]
-#                 break
-#     ret = 0
-#     for i in range(r):
-#         ret += B**(r - i - 1) * indices[r]
-#     return ret
-
-
-
-# """
-# Let's make sure the code above works! 
-#"""
+    indices = []
+    for j in range(r):
+        #print(features[0][j], quantiles_list[j][0])
+        for i in range(B):
+            #print(features[0][j], quantiles_list[j][0][i], quantiles_list[j][0][i + 1])
+            if features[0][j] >= quantiles_list[j][0][i] and features[0][j] <= quantiles_list[j][0][i + 1]:
+                indices.append(i)
+                break
+        assert(len(indices) == j + 1)
+    ret = 0
+    for i in range(r):
+        ret += B**(r - i - 1) * indices[i]
+    return ret
 
 
-
-# # Use get_set to construct f hat using the D2 values
-# def fhat(sample):
-#     set_num = get_set(sample)
-#     counts = np.sum(sets[set_num][1])
-#     size = set_counts[set_num]
-#     return counts / size
+def fhat(sample):
+    """Construct fhat from D2 samples"""
+    set_num = get_set(sample)
+    counts = np.sum(sets[set_num][1])
+    size = set_counts[set_num]
+    return counts / size
 
 # # Finally, evaluate the predictor on the test data
 
     
+
+
+""" Run some basic checks...
+# Test get_data()
+D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test, test=True)
+assert(len(x_valid) == len(y_valid) == n_valid)
+assert(len(x_test) == len(y_test) == n_test)
+assert(len(D1[0]) == len(D1[1]) == n_train // 2)
+assert(len(D2[0]) == len(D2[1]) == n_train // 2)
+assert(not(np.all(np.equal(D1[0], D2[0]))))
+
+# Test train_model() and get_model()
+D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test)
+model = get_model(d, r, eps)
+print(model.summary())
+mse = train_model(model, D1, x_valid, y_valid)
+
+# Test split_sets() and get_set()
+r = 3
+d = 3
+B = 4
+D1, D2, x_valid, y_valid, x_test, y_test = get_data(r, d, C, n_train, n_valid, n_test, test=True)
+model = get_model(d, r, eps)
+mse = train_model(model, D1, x_valid, y_valid)
+sets, set_counts, quantiles_list = split_sets(model, D1, C, B, r)
+model_top = keras.Model(inputs=model.input, outputs=model.get_layer('dense').output)
+
+# Check that get_sets returns the set that the sample actually came from
+for i in range(len(sets)):
+    for j in range(len(sets[i][0])):
+        x = sets[i][0][j]
+        #print("x", np.shape(x), x)
+        get = get_set(x, r, model, quantiles_list)
+        if len(x) != 0 and get != i:
+            #print("Bad:", get, i, model_top.predict(x.reshape((1, -1))), quantiles_list)
+"""
+
 
