5f731e47dbb417ca409edd773c94c50c61d842d5
mburhanpurkar <maya.burhanpurkar@gmail.com>
2021-06-04 21:24:04 -0400

    reorganize data generation

--- code/resnet/generate_data.py
+++ code/resnet/generate_data.py
@@ -4,8 +4,7 @@ import tensorflow as tf
 
 # NB: the p -> 1-p error has been fixed in the code below. This is NOT
 # the original code that was used for the end of May experiments.
-# Data generation script for the hybrid tests can be found in the data
-# generation notebook.
+
 
 # Make data for 1/x^2 testing
 os.mkdir('data_squared')
@@ -74,3 +73,356 @@ np.save('data/y_test', y_test)
 np.save('data/y_train_old', y_train_old)
 np.save('data/y_test_old', y_test_old)
 
+
+# Make data for hybrid test
+os.mkdir("data_hybrids_uniform")
+os.mkdir("data_hybrids_fixed")
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+test = pd.DataFrame(data=np.ravel(y_test), columns=['y'])
+train = pd.DataFrame(data=np.ravel(y_train), columns=['y'])
+
+n = 500
+images_arr = np.empty((n * 10, 32, 32, 3))
+labels_arr = np.empty(n * 10)
+images_arr_pure = np.empty((n * 10, 32, 32, 3))
+labels_arr_pure = np.empty(n * 10)
+original_labels = [None] * n * 10
+original_labels_pure = [None] * n * 10
+n_disp = 4
+
+for i in range(10):
+    indices_1 = train[train['y'] == i].index.to_numpy()
+    indices_2 = train[train['y'] == (i + 1) % 10].index.to_numpy()
+    
+    # Get the pure ones
+    images_arr_pure[i * n : (i + 1) * n] = x_train[indices_1[:n]]
+    labels_arr_pure[i * n : (i + 1) * n] = np.ones(n) * i / 10.
+    original_labels_pure[i * n : (i + 1) * n] = [str(i) + "_1"] * n
+
+    # Remove the first n
+    indices_1 = indices_1[n:]
+    indices_2 = indices_2[n:]
+
+    # Randomly sample n of the indices 
+    indices_1 = np.random.choice(indices_1, size=n, replace=True)
+    indices_2 = np.random.choice(indices_2, size=n, replace=True)
+
+    # Generate n lambdas
+    hybrids = np.random.uniform(size=n)
+    lambdas = [[k] for k in hybrids]
+    lambdas = np.tile(lambdas, (32 * 32 * 3)).reshape(n, 32, 32, 3)
+
+    # Compute the probabilities 
+    probs = (lambdas * i + (1 - lambdas) * (i + 1) % 10) / 10.
+
+    # Create the hybrids 
+    images = x_train[indices_1] * lambdas + x_train[indices_2] * (1 - lambdas)
+
+    # Append images and labels to array
+    images_arr[i * n : (i + 1) * n] = images
+    labels_arr[i * n : (i + 1) * n] = [probs[k, 0, 0, 0] for k in range(len(probs))]
+    for k in range(n):
+        original_labels[i * n + k] = str(i) + "_" + str(hybrids[k])
+        
+    # Display the images
+    fig, ax = plt.subplots(nrows=n_disp, ncols=3, figsize=(4*3, 4 * n_disp))
+    for i_disp in range(n_disp):
+        ax[i_disp, 0].imshow(images[i_disp] /255.)
+        ax[i_disp, 0].set_title(str(round(round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[i] + " " \
+                        + str(round(100 - round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[(i + 1) % 10] + " (" + \
+                           str(round(100 * round(probs[i_disp, 0, 0, 0], 3), 3)) + "%)")
+        ax[i_disp, 1].imshow(x_train[indices_1[i_disp]])
+        ax[i_disp, 1].set_title(labels[i])
+        ax[i_disp, 2].imshow(x_train[indices_2[i_disp]])
+        ax[i_disp, 2].set_title(labels[(i + 1) % 10])
+        for j in range(3):
+            ax[i_disp, j].axis('off')
+        assert(probs[i_disp, 0, 0, 0] == labels_arr[i * n + i_disp])
+    plt.tight_layout()
+    plt.show()
+
+    
+# Do sampling and converting to one-hot
+samples_arr = np.random.binomial(1, labels_arr)
+samples_arr = tf.keras.utils.to_categorical(samples_arr, num_classes=2)
+categorical_labels_arr = np.empty((len(labels_arr), 2))
+categorical_labels_arr[:, 1] = labels_arr
+categorical_labels_arr[:, 0] = 1 - labels_arr
+
+samples_arr_pure = np.random.binomial(1, labels_arr_pure)
+samples_arr_pure = tf.keras.utils.to_categorical(samples_arr_pure, num_classes=2)
+categorical_labels_arr_pure = np.empty((len(labels_arr_pure), 2))
+categorical_labels_arr_pure[:, 1] = labels_arr_pure
+categorical_labels_arr_pure[:, 0] = 1 - labels_arr_pure
+
+# Concatenate
+images_arr = np.concatenate((images_arr, images_arr_pure))
+categorical_labels_arr = np.concatenate((categorical_labels_arr, categorical_labels_arr_pure))
+samples_arr = np.concatenate((samples_arr, samples_arr_pure))
+original_labels = np.concatenate((original_labels, original_labels_pure))
+
+# Save
+np.save("data_hybrids_uniform/train_images.npy", images_arr)
+np.save("data_hybrids_uniform/train_probs.npy", categorical_labels_arr)
+np.save("data_hybrids_uniform/train_labels.npy", samples_arr)
+np.save("data_hybrids_uniform/train_original_labels.npy", original_labels)
+
+n = 500
+images_arr = np.empty((n * 10, 32, 32, 3))
+labels_arr = np.empty(n * 10)
+images_arr_pure = np.empty((n * 10, 32, 32, 3))
+labels_arr_pure = np.empty(n * 10)
+original_labels = [None] * n * 10
+original_labels_pure = [None] * n * 10
+n_disp = 4
+
+for i in range(10):
+    indices_1 = test[test['y'] == i].index.to_numpy()
+    indices_2 = test[test['y'] == (i + 1) % 10].index.to_numpy()
+    
+    # Get the pure ones
+    images_arr_pure[i * n : (i + 1) * n] = x_test[indices_1[:n]]
+    labels_arr_pure[i * n : (i + 1) * n] = np.ones(n) * i / 10.
+    original_labels_pure[i * n : (i + 1) * n] = [str(i) + "_1"] * n
+
+    # Remove the first n
+    indices_1 = indices_1[n:]
+    indices_2 = indices_2[n:]
+
+    # Randomly sample n of the indices 
+    indices_1 = np.random.choice(indices_1, size=n, replace=True)
+    indices_2 = np.random.choice(indices_2, size=n, replace=True)
+
+    # Generate n lambdas
+    hybrids = np.random.uniform(size=n)
+    lambdas = [[k] for k in hybrids]
+    lambdas = np.tile(lambdas, (32 * 32 * 3)).reshape(n, 32, 32, 3)
+
+    # Compute the probabilities 
+    probs = (lambdas * i + (1 - lambdas) * (i + 1) % 10) / 10.
+
+    # Create the hybrids 
+    images = x_test[indices_1] * lambdas + x_test[indices_2] * (1 - lambdas)
+
+    # Append images and labels to array
+    images_arr[i * n : (i + 1) * n] = images
+    labels_arr[i * n : (i + 1) * n] = [probs[k, 0, 0, 0] for k in range(len(probs))]
+    for k in range(n):
+        original_labels[i * n + k] = str(i) + "_" + str(hybrids[k])
+        
+    # Display the images
+    fig, ax = plt.subplots(nrows=n_disp, ncols=3, figsize=(4*3, 4 * n_disp))
+    for i_disp in range(n_disp): 
+        ax[i_disp, 0].imshow(images[i_disp] / 255.)
+        ax[i_disp, 0].set_title(str(round(round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[i] + " " \
+                        + str(round(100 - round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[(i + 1) % 10] + " (" + \
+                           str(round(100 * round(probs[i_disp, 0, 0, 0], 3), 3)) + "%)")
+        ax[i_disp, 1].imshow(x_test[indices_1[i_disp]])
+        ax[i_disp, 1].set_title(labels[i])
+        ax[i_disp, 2].imshow(x_test[indices_2[i_disp]])
+        ax[i_disp, 2].set_title(labels[(i + 1) % 10])
+        for j in range(3):
+            ax[i_disp, j].axis('off')
+        assert(probs[i_disp, 0, 0, 0] == labels_arr[i * n + i_disp])
+    plt.tight_layout()
+    plt.show()
+
+    
+# Do sampling and converting to one-hot
+samples_arr = np.random.binomial(1, labels_arr)
+samples_arr = tf.keras.utils.to_categorical(samples_arr, num_classes=2)
+categorical_labels_arr = np.empty((len(labels_arr), 2))
+categorical_labels_arr[:, 1] = labels_arr
+categorical_labels_arr[:, 0] = 1 - labels_arr
+
+samples_arr_pure = np.random.binomial(1, labels_arr_pure)
+samples_arr_pure = tf.keras.utils.to_categorical(samples_arr_pure, num_classes=2)
+categorical_labels_arr_pure = np.empty((len(labels_arr_pure), 2))
+categorical_labels_arr_pure[:, 1] = labels_arr_pure
+categorical_labels_arr_pure[:, 0] = 1 - labels_arr_pure
+
+# Concatenate
+images_arr = np.concatenate((images_arr, images_arr_pure))
+categorical_labels_arr = np.concatenate((categorical_labels_arr, categorical_labels_arr_pure))
+samples_arr = np.concatenate((samples_arr, samples_arr_pure))
+original_labels = np.concatenate((original_labels, original_labels_pure))
+
+# Save
+np.save("data_hybrids_uniform/test_images.npy", images_arr)
+np.save("data_hybrids_uniform/test_probs.npy", categorical_labels_arr)
+np.save("data_hybrids_uniform/test_labels.npy", samples_arr)
+np.save("data_hybrids_uniform/test_original_labels.npy", original_labels)
+
+n = 500
+images_arr = np.empty((n * 10, 32, 32, 3))
+labels_arr = np.empty(n * 10)
+images_arr_pure = np.empty((n * 10, 32, 32, 3))
+labels_arr_pure = np.empty(n * 10)
+original_labels = [None] * n * 10
+original_labels_pure = [None] * n * 10
+n_disp = 4
+
+for i in range(10):
+    indices_1 = train[train['y'] == i].index.to_numpy()
+    indices_2 = train[train['y'] == (i + 1) % 10].index.to_numpy()
+    
+    # Get the pure ones
+    images_arr_pure[i * n : (i + 1) * n] = x_train[indices_1[:n]]
+    labels_arr_pure[i * n : (i + 1) * n] = np.ones(n) * i / 10.
+    original_labels_pure[i * n : (i + 1) * n] = [str(i) + "_1"] * n
+
+    # Remove the first n
+    indices_1 = indices_1[n:]
+    indices_2 = indices_2[n:]
+
+    # Randomly sample n of the indices 
+    indices_1 = np.random.choice(indices_1, size=n, replace=True)
+    indices_2 = np.random.choice(indices_2, size=n, replace=True)
+
+    # Generate n lambdas
+    hybrids = np.random.choice(np.linspace(0, 1, 11)[1:-1], size=n, replace=True)
+    lambdas = [[k] for k in hybrids]
+    lambdas = np.tile(lambdas, (32 * 32 * 3)).reshape(n, 32, 32, 3)
+
+    # Compute the probabilities 
+    probs = (lambdas * i + (1 - lambdas) * (i + 1) % 10) / 10.
+
+    # Create the hybrids 
+    images = x_train[indices_1] * lambdas + x_train[indices_2] * (1 - lambdas)
+
+    # Append images and labels to array
+    images_arr[i * n : (i + 1) * n] = images
+    labels_arr[i * n : (i + 1) * n] = [probs[k, 0, 0, 0] for k in range(len(probs))]
+    for k in range(n):
+        original_labels[i * n + k] = str(i) + "_" + str(hybrids[k])
+        
+    # Display the images
+    fig, ax = plt.subplots(nrows=n_disp, ncols=3, figsize=(4*3, 4 * n_disp))
+    for i_disp in range(n_disp):
+        ax[i_disp, 0].imshow(images[i_disp] /255.)
+        ax[i_disp, 0].set_title(str(round(round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[i] + " " \
+                        + str(round(100 - round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[(i + 1) % 10] + " (" + \
+                           str(round(100 * round(probs[i_disp, 0, 0, 0], 3), 3)) + "%)")
+        ax[i_disp, 1].imshow(x_train[indices_1[i_disp]])
+        ax[i_disp, 1].set_title(labels[i])
+        ax[i_disp, 2].imshow(x_train[indices_2[i_disp]])
+        ax[i_disp, 2].set_title(labels[(i + 1) % 10])
+        for j in range(3):
+            ax[i_disp, j].axis('off')
+        assert(probs[i_disp, 0, 0, 0] == labels_arr[i * n + i_disp])
+    plt.tight_layout()
+    plt.show()
+
+    
+# Do sampling and converting to one-hot
+samples_arr = np.random.binomial(1, labels_arr)
+samples_arr = tf.keras.utils.to_categorical(samples_arr, num_classes=2)
+categorical_labels_arr = np.empty((len(labels_arr), 2))
+categorical_labels_arr[:, 1] = labels_arr
+categorical_labels_arr[:, 0] = 1 - labels_arr
+
+samples_arr_pure = np.random.binomial(1, labels_arr_pure)
+samples_arr_pure = tf.keras.utils.to_categorical(samples_arr_pure, num_classes=2)
+categorical_labels_arr_pure = np.empty((len(labels_arr_pure), 2))
+categorical_labels_arr_pure[:, 1] = labels_arr_pure
+categorical_labels_arr_pure[:, 0] = 1 - labels_arr_pure
+
+# Concatenate
+images_arr = np.concatenate((images_arr, images_arr_pure))
+categorical_labels_arr = np.concatenate((categorical_labels_arr, categorical_labels_arr_pure))
+samples_arr = np.concatenate((samples_arr, samples_arr_pure))
+original_labels = np.concatenate((original_labels, original_labels_pure))
+
+# Save
+np.save("data_hybrids_fixed/train_images.npy", images_arr)
+np.save("data_hybrids_fixed/train_probs.npy", categorical_labels_arr)
+np.save("data_hybrids_fixed/train_labels.npy", samples_arr)
+np.save("data_hybrids_fixed/train_original_labels.npy", original_labels)
+
+n = 500
+images_arr = np.empty((n * 10, 32, 32, 3))
+labels_arr = np.empty(n * 10)
+images_arr_pure = np.empty((n * 10, 32, 32, 3))
+labels_arr_pure = np.empty(n * 10)
+original_labels = [None] * n * 10
+original_labels_pure = [None] * n * 10
+n_disp = 4
+
+for i in range(10):
+    indices_1 = test[test['y'] == i].index.to_numpy()
+    indices_2 = test[test['y'] == (i + 1) % 10].index.to_numpy()
+    
+    # Get the pure ones
+    images_arr_pure[i * n : (i + 1) * n] = x_test[indices_1[:n]]
+    labels_arr_pure[i * n : (i + 1) * n] = np.ones(n) * i / 10.
+    original_labels_pure[i * n : (i + 1) * n] = [str(i) + "_1"] * n
+
+    # Remove the first n
+    indices_1 = indices_1[n:]
+    indices_2 = indices_2[n:]
+
+    # Randomly sample n of the indices 
+    indices_1 = np.random.choice(indices_1, size=n, replace=True)
+    indices_2 = np.random.choice(indices_2, size=n, replace=True)
+
+    # Generate n lambdas
+    hybrids = np.random.choice(np.linspace(0, 1, 11)[1:-1], size=n, replace=True)
+    lambdas = [[k] for k in hybrids]
+    lambdas = np.tile(lambdas, (32 * 32 * 3)).reshape(n, 32, 32, 3)
+
+    # Compute the probabilities 
+    probs = (lambdas * i + (1 - lambdas) * (i + 1) % 10) / 10.
+
+    # Create the hybrids 
+    images = x_test[indices_1] * lambdas + x_test[indices_2] * (1 - lambdas)
+
+    # Append images and labels to array
+    images_arr[i * n : (i + 1) * n] = images
+    labels_arr[i * n : (i + 1) * n] = [probs[k, 0, 0, 0] for k in range(len(probs))]
+    for k in range(n):
+        original_labels[i * n + k] = str(i) + "_" + str(hybrids[k])
+        
+    # Display the images
+    fig, ax = plt.subplots(nrows=n_disp, ncols=3, figsize=(4*3, 4 * n_disp))
+    for i_disp in range(n_disp): 
+        ax[i_disp, 0].imshow(images[i_disp] / 255.)
+        ax[i_disp, 0].set_title(str(round(round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[i] + " " \
+                        + str(round(100 - round(lambdas[i_disp, 0, 0, 0], 3) * 100, 3)) + "% " + labels[(i + 1) % 10] + " (" + \
+                           str(round(100 * round(probs[i_disp, 0, 0, 0], 3), 3)) + "%)")
+        ax[i_disp, 1].imshow(x_test[indices_1[i_disp]])
+        ax[i_disp, 1].set_title(labels[i])
+        ax[i_disp, 2].imshow(x_test[indices_2[i_disp]])
+        ax[i_disp, 2].set_title(labels[(i + 1) % 10])
+        for j in range(3):
+            ax[i_disp, j].axis('off')
+        assert(probs[i_disp, 0, 0, 0] == labels_arr[i * n + i_disp])
+    plt.tight_layout()
+    plt.show()
+
+    
+# Do sampling and converting to one-hot
+samples_arr = np.random.binomial(1, labels_arr)
+samples_arr = tf.keras.utils.to_categorical(samples_arr, num_classes=2)
+categorical_labels_arr = np.empty((len(labels_arr), 2))
+categorical_labels_arr[:, 1] = labels_arr
+categorical_labels_arr[:, 0] = 1 - labels_arr
+
+samples_arr_pure = np.random.binomial(1, labels_arr_pure)
+samples_arr_pure = tf.keras.utils.to_categorical(samples_arr_pure, num_classes=2)
+categorical_labels_arr_pure = np.empty((len(labels_arr_pure), 2))
+categorical_labels_arr_pure[:, 1] = labels_arr_pure
+categorical_labels_arr_pure[:, 0] = 1 - labels_arr_pure
+
+# Concatenate
+images_arr = np.concatenate((images_arr, images_arr_pure))
+categorical_labels_arr = np.concatenate((categorical_labels_arr, categorical_labels_arr_pure))
+samples_arr = np.concatenate((samples_arr, samples_arr_pure))
+original_labels = np.concatenate((original_labels, original_labels_pure))
+
+# Save
+np.save("data_hybrids_fixed/test_images.npy", images_arr)
+np.save("data_hybrids_fixed/test_probs.npy", categorical_labels_arr)
+np.save("data_hybrids_fixed/test_labels.npy", samples_arr)
+np.save("data_hybrids_fixed/test_original_labels.npy", original_labels)
---
+++ code/resnet/generate_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -J data_gen
+#SBATCH -n 1                # Number of cores
+#SBATCH -t 0-00:20           # Runtime in D-HH:MM
+#SBATCH -p shared           # Partition to submit to
+#SBATCH --mem=1G
+#SBATCH --account=dwork_lab
+#SBATCH --output=data_gen
+#SBATCH --mail-user=maya.burhanpurkar@gmail.com
+#SBATCH --mail-type=ALL
+
+module purge
+module load Anaconda3/2019.10
+source activate tf_mult
+
+
+# Model parameter
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | --c---     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
+
+python generate_data.py
+
+conda deactivate
